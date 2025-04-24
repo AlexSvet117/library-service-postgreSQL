9aa4bccc0ce7a7b84ee4f92e0539dca53042acaf
AlexSvet117 <alex.svet117@gmail.com>
2025-04-24 01:50:10 +0100

    app error handler methods update

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def create_app():
     def bad_request(error):
         return jsonify({"error" : "Bad Request"}), 400
     
-    @app.errorhandler(404)
-    def bad_request(error):
+    @app.errorhandler(404) 
+    def not_found(error):
         return jsonify({"error" : "Not Found"}), 404
     
     @app.errorhandler(500)
-    def bad_request(error):
+    def internal_error(error):
         return jsonify({"error" : "Internal Server Error"}), 500
 
 
