7909c138e97ece4237c917ca70b23dbdcfd83acd
AlexSvet117 <alex.svet117@gmail.com>
2025-04-24 20:59:38 +0100

    get_db and close_db functions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from controllers.book_controller import book_bp
+from db import close_db
 
 
 def create_app():
@@ -17,13 +18,12 @@ def create_app():
     @app.errorhandler(500)
     def internal_error(error):
         return jsonify({"error" : "Internal Server Error"}), 500
+    
 
-
+    app.teardown_appcontext(close_db)
 
 
     return app
-
-
 
 
 if __name__ == "__main__":
--- db.py
+++ db.py
@@ -28,4 +28,17 @@ book_data = [book1, book2]
 
 
 def get_db():
-    
+    if 'db' not in g:
+        g.db = psycopg2.connect(
+            dbname ="flask_postgres_db", 
+            user ="postgres",
+            password ="postgres",
+            host="localhost",
+            port="5432"
+        )
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
